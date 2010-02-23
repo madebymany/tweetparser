4fed86f82335eebdc70286c2aee82946f39773f7
Paul Battley <pbattley@gmail.com>
2010-02-23 15:35:44 +0000

    Wrapper to make loading less onerous.

---
+++ lib/tweetparser.rb
@@ -0,0 +1,3 @@
+require "treetop"
+require "polyglot"
+require "tweetparser/grammar"
--- test.rb
+++ test.rb
@@ -1,7 +1,5 @@
 $:.unshift(File.join(File.dirname(__FILE__), "lib"))
-require "treetop"
-require "polyglot"
-require "tweetparser/grammar"
+require "tweetparser"
 
 s = %{Another test:  <a href="http://twitpic.com/14vzny" target="_blank"><img src="http://twitpic.com/show/mini/14vzny" /></a>
 http://twitpic.com/14vzny 3 http://twitpic.com/14vzny}
